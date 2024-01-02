a73447a5a6cf93cec95731ae7feaaa09fc2201b2
azahinhasan <azahinhasan@gmail.com>
2024-01-02 21:53:12 +0600

    refactor: add label ,label color for and date in shift schema

--- backend/models/shift.model.js
+++ backend/models/shift.model.js
@@ -1,19 +1,36 @@
 const mongoose = require("mongoose");
 
 const ShiftSchema = new mongoose.Schema({
-  startTime: {
+  label: {
+    //shift name
+    type: String,
+  },
+  label_color: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    required: [true, "Please Start Date"],
+  },
+  // end_date: {
+  //   type: Date,
+  //   required: [true, "Please End Date"],
+  // },
+  start_time: {
     type: Date,
     required: [true, "Please Start Time"],
   },
-  endTime: {
+  end_time: {
     type: Date,
     required: [true, "Please End Time"],
   },
-  assignedEmployees: {
-    type: mongoose.Schema.ObjectId,
-    ref: "users",
-    required: [true, "Please assign Employee"],
-  },
+  assigned_employee: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "users",
+      required: [true, "Please assign Employee"],
+    },
+  ],
 });
 
 module.exports = mongoose.model("shifts", ShiftSchema);
