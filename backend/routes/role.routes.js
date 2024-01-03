58a2f203ca668581f94da47fb84771c7fbeedc4e
azahinhasan <azahinhasan@gmail.com>
2024-01-03 12:56:32 +0600

    refactor: folder name change backend to server

--- backend/routes/role.routes.js
+++
@@ -1,9 +0,0 @@
-const roleCtrl = require("../controllers/role.controller");
-const express = require("express");
-const authCheck = require("../middleware/auth_check");
-const router = express.Router();
-
-router.route("/get_all").get(authCheck.haveAccess, roleCtrl.getAllRole);
-router.route("/create").post(authCheck.haveAccess, roleCtrl.createRole);
-
-module.exports = router;
