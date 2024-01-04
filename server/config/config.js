1af5075ee1703521a78227224bc522e33d8c2679
azahinhasan <azahinhasan@gmail.com>
2024-01-04 21:55:33 +0600

    config: dockerized the app

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const config = {
   ENV: process.env.ENV || "development",
-  PORT: process.env.PORT || 5000,
-  JWT_SECRET: process.env.JWT_SECRET||"fasdfasd",
+  PORT: 5000,
+  JWT_SECRET: process.env.JWT_SECRET,
   MONGO_URI: process.env.MONGO_URI||"mongodb+srv://admin:rk832YNjTWiv4l6I@cluster0.u3psp.mongodb.net/singularity_task?retryWrites=true&w=majority",
 };
 
